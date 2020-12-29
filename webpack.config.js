9b1d6400bd5877ef36c8aec3f6f6a6987c85a76f
pgalvezp72 <76529792+pgalvezp72@users.noreply.github.com>
2020-12-29 20:48:40 +0100

    last changes of the day

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
 const path = require ('path')
 
+const postCSSPlugins = [
+    require ('postcss-simple-vars'),
+    require ('postcss-nested'),
+    require ('autoprefixer')
+]
+
 module.exports= {
-    entry: './app/assets/images/scripts/App.js', 
-    output: {
-        filename: 'bundled.js',
-        path: path.resolve(__dirname,'app')
-    },
-    mode: 'development',
-    watch: true
+entry: './app/assets/images/scripts/App.js', 
+output: {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname,'app')
+},
+mode: 'development',
+watch: true,
+module: {
+    rules: [
+        {
+        test: /.css$/i,
+        use: ['style-loader','css-loader',{loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+    }
+    ]
+}
 
 }
